c239275d8993f58a67b7982ba45d73b821d2e9f8
SAR-Khadem <sar.khadem2000@gmail.com>
2020-08-26 22:23:03 +0430

    Expense and Income are there :)

--- web/models.py
+++ web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.IntegerField()
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.IntegerField()
