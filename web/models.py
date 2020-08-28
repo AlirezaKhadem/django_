21a31173068d05cb3dfe8489020ea3b4119b25cf
asus@DESKTOP-CL0DLB1 <sar.khadem2000@gmail.com>
2020-08-28 20:56:03 +0430

    add user and token

--- web/models.py
+++ web/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    token = models.CharField(max_length=48)
+
 
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return "{} ** {} | {}".format(self.user, self.date, self.amount)
 
 
 class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return "{} ** {} | {}".format(self.user, self.date, self.amount)
--- web/views.py
+++ web/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def submit_expense(request):
+    """user submit a expense"""
+    print('we are here')
